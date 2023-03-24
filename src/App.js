c455af74af1312be18d6f28583063c3d30e20ca8
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-03-24 20:08:49 +0100

    scaffolding project from CRA

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 const App = () =>
-  <div>
-    hello world
+  <div className="categories-container">
+    <div className="category-container">
+      {/* img */ }
+      <div className="category-body-container">
+        <h2>Livestocks</h2>
+        <p>View More</p>
+      </div>
+    </div>
+
+    <div className="category-container">
+      {/* img */ }
+      <div className="category-body-container">
+        <h2>Vegetables</h2>
+        <p>View More</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */ }
+      <div className="category-body-container">
+        <h2>Fruits</h2>
+        <p>View More</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */ }
+      <div className="category-body-container">
+        <h2>Cereals</h2>
+        <p>View More</p>
+      </div>
+    </div>
+    <div className="category-container">
+      {/* img */ }
+      <div className="category-body-container">
+        <h2>Spices</h2>
+        <p>View More</p>
+      </div>
+    </div>
   </div>
 
 export default App;
