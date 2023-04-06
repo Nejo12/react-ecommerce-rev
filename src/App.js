b1fca6fcbf5aa72e58e7b515fc4993ca8ef9ac4c
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-06 11:03:26 +0200

    add react-router to proceed with navigation. adjusted the page title and fonts

--- src/App.js
+++ src/App.js
@@ -2,39 +2,31 @@ import Directory from './components/directory/directory.component'
 
 const App = () => {
 
-	const categories = [
-		{
-			id: 1,
-			title: 'Livestock',
-			imageUrl: 'https://i.ibb.co/mXRFyTk/diary-cow.jpg'
-		},
-		{
-			id: 2,
-			title: 'Vegetables',
-			imageUrl: 'https://i.ibb.co/nQ5Kbjh/green-cabbage.jpg'
-		},
-		{
-			id: 3,
-			title: 'Spices',
-			imageUrl: 'https://i.ibb.co/0YnPYLX/assorted-spices.jpg'
-		},
-		{
-			id: 4,
-			title: 'Cereals',
-			imageUrl: 'https://i.ibb.co/vVWJWVh/cereals.jpg'
-		},
-		{
-			id: 5,
-			title: 'Fruits',
-			imageUrl: 'https://i.ibb.co/xf5zp8J/Raspberry.jpg'
-		},
-	]
-
-	// https://i.ibb.co/vVWJWVh/cereals.jpg
-	// https://i.ibb.co/5MfrXkV/fruits.jpg
-	// https://i.ibb.co/D9pf6Yt/livestock.jpg
-	// https://i.ibb.co/7jhRB0v/spices.jpg
-	// https://i.ibb.co/x2XwkK0/vegetables.jpg
+	const categories = [ {
+		id: 1,
+		title: 'Livestock',
+		imageUrl: 'https://i.ibb.co/mXRFyTk/diary-cow.jpg'
+	},
+	{
+		id: 2,
+		title: 'Vegetables',
+		imageUrl: 'https://i.ibb.co/nQ5Kbjh/green-cabbage.jpg'
+	},
+	{
+		id: 3,
+		title: 'Spices',
+		imageUrl: 'https://i.ibb.co/0YnPYLX/assorted-spices.jpg'
+	},
+	{
+		id: 4,
+		title: 'Cereals',
+		imageUrl: 'https://i.ibb.co/vVWJWVh/cereals.jpg'
+	},
+	{
+		id: 5,
+		title: 'Fruits',
+		imageUrl: 'https://i.ibb.co/xf5zp8J/Raspberry.jpg'
+	} ]
 
 	return (
 		<Directory categories={ categories } />
