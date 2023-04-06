282cf71cac64c2d584fbb0ba200d005e3edc19e4
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-06 15:15:53 +0200

    initial styling for navigation + naigation logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,18 @@
 import { Fragment } from "react"
 import { Outlet, Link } from "react-router-dom"
 
+import './navigation.styles.scss'
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to='/'>
-          <div>Logo</div>
+          <img
+            className='logo'
+            src='https://i.ibb.co/hZQj6np/mill-157613-640.png'
+            alt='nav-logo'
+          />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to='/shop'>
