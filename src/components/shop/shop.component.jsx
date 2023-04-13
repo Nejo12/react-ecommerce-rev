37429510cf60df86dfc9c60785af0df2aa2a275e
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-13 11:53:48 +0200

    Product Card component
    - create products card component to design the products.

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -2,13 +2,14 @@ import { useContext } from "react"
 
 import { ProductsContext } from "../../contexts/products.context"
 
+import ProductCard from '../product-card/product-card.component';
+import './shop.styles.scss';
+
 const Shop = () => {
   const { products } = useContext(ProductsContext)
   return (
-    <div>{ products.map(({ id, title }) => (
-      <div key={ id }>
-        <h1>{ title }</h1>
-      </div>
+    <div className="products-container">{ products.map((product) => (
+      <ProductCard key={ product.id } product={ product } />
     )) }</div>
   )
 }
