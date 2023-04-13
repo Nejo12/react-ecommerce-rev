abca6871937aa657725e1f394449a85329ac66b4
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-13 13:22:23 +0200

    adding products to cart
    - image, name, price and qty is shown on cart-card

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,53 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+
+// helper function to find and increase qtty from existing array that match id of the productToAdd, otherwise, create new item in cart
+export const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  )
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    )
+  }
+
+  return [ ...cartItems, { ...productToAdd, quantity: 1 } ]
+}
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => { },
+  cartItems: [],
+  addItemToCart: () => { },
+  cartItemCount: 0,
 })
 
 export const CartProvider = ({ children }) => {
   const [ isCartOpen, setIsCartOpen ] = useState(false)
+  const [ cartItems, setCartItems ] = useState([])
+  const [ cartItemCount, setCartItemCount ] = useState(0);
 
-  const value = { isCartOpen, setIsCartOpen }
+  useEffect(() => {
+    const count = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartItemCount(count);
+  }, [ cartItems ]);
+
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    cartItemCount,
+  };
 
   return <CartContext.Provider value={ value }>{ children }</CartContext.Provider>
 }
